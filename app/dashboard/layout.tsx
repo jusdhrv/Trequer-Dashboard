68495e0c9ae78aa92400a5f0e9c5544fea544184
jusdhrv <20d.dhruv@gmail.com>
2025-04-13 12:24:37 +0530

    Fix 'Sidebar' component importing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,18 @@
-'use client'
+"use client";
 
-import Sidebar from "../../components/Sidebar"
+import { Sidebar } from "../../components/Sidebar";
 
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <div className="h-screen w-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-background">
         <Sidebar />
       </div>
-      <main className="md:pl-72">
-        {children}
-      </main>
+      <main className="md:pl-72">{children}</main>
     </div>
-  )
+  );
 }
-
